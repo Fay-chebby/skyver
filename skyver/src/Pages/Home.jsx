079c6ea9811874698196c2d82669928c1b3b09c4
Fay-chebby <chebetf034@gmail.com>
2025-05-24 22:04:32 +0300

    home page

---
+++ skyver/src/Pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '../../styles/home/Home.module.css';
+import Navbar from '../../components/Navbar';
+
+function Home() {
+    return (
+        <div className={styles.home}>
+            <Navbar />
+            <div className={styles.heroSection}>
+                <div className={styles.heroText}>
+                    <h1>The Power of a Growth Mindset</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula.</p>
+                    <button className={styles.viewMoreBtn}>View more</button>
+                </div>
+                <div className={styles.heroImage}>
+                    <img src="https://via.placeholder.com/400x400" alt="Speaker" />
+                    <div className={styles.successBox}>
+                        <div className={styles.percentage}>90%</div>
+                        <div>Success<br />Result</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
