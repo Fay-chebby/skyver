f3491660cb84fd3388516ce4c47e6864b2f951a8
Fay-chebby <chebetf034@gmail.com>
2025-05-24 21:50:21 +0300

    structure of the files

---
+++ skyver/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">SKYVER</div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+            <button className="contact-btn">Contact Us</button>
+        </nav>
+    );
+};
+
+export default Navbar;
