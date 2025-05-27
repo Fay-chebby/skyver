60fe94c4d18f9f4134efa319c817d5cac3c6466a
Fay-chebby <chebetf034@gmail.com>
2025-05-27 14:39:50 +0300

    image styling

--- skyver/src/App.jsx
+++ skyver/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Home from "./Pages/Home/Home.jsx";
+import About from "./Pages/about page/About.jsx";
 
 function App() {
 
 
   return (
     <Home/>
+  <About/>
   )
 }
 
--- skyver/src/Pages/about page/About.jsx
+++ skyver/src/Pages/about page/About.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./HeroImage.module.css";
-import heroImg from "../"; // adjust path as needed
+import heroImg from "../";
+import Navbar from "../../Components/Navbar.jsx"; // adjust path as needed
 
 const About = () => {
     return (
+        <div>
+            <Navbar />
         <div className={styles.hero}>
             <img src={heroImg} alt="Speaker" className={styles.heroImage} />
+        </div>
         </div>
     );
 };
